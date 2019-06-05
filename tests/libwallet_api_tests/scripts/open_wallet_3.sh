8fdf4c4dda9ef0ed1fc3c95d74b7585770e141cf
txchangecoin-project <49847742+txchangecoin-project@users.noreply.github.com>
2019-06-05 08:14:10 +0100

    Changed port from 38081 to 38387

--- tests/libwallet_api_tests/scripts/open_wallet_3.sh
+++ tests/libwallet_api_tests/scripts/open_wallet_3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap txchangecoin-wallet-cli --wallet-file wallet_03.bin --password "" --testnet --trusted-daemon --daemon-address localhost:38081  --log-file wallet_03.log
+rlwrap txchangecoin-wallet-cli --wallet-file wallet_03.bin --password "" --testnet --trusted-daemon --daemon-address localhost:38387  --log-file wallet_03.log
 

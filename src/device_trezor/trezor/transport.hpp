1131c842ef6f2bd8b7b51d17ee17f2a63033be5e
txchangecoin-project <49847742+txchangecoin-project@users.noreply.github.com>
2019-06-16 21:31:11 +0100

    Update transport.hpp

--- src/device_trezor/trezor/transport.hpp
+++ src/device_trezor/trezor/transport.hpp
@@ -80,7 +80,7 @@ namespace trezor {
     t_serialize(out_struct, req_param);
 
     http::fields_list additional_params;
-    additional_params.push_back(std::make_pair("Origin","https://monero.trezor.io"));
+    additional_params.push_back(std::make_pair("Origin","https://txchangecoin.trezor.io"));
     additional_params.push_back(std::make_pair("Content-Type","application/json; charset=utf-8"));
 
     const http::http_response_info* pri = nullptr;
